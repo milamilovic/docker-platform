a190319592d0df2b77f3ba36e56dd6e5258abec1
Rade Pejanović <radepejanovic@gmail.com>
2026-01-08 21:59:32 +0100

    add initial Spring Boot project #1

---
+++ backend/src/main/java/com/dockerplatform/backend/DockerPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dockerplatform.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPlatformBackendApplication.class, args);
+	}
+
+}
