5b44dd75029eda79440fbeb8a451ca809455ac5f
Rade Pejanović <radepejanovic@gmail.com>
2026-01-09 20:04:28 +0100

    add dummy component to test connection with backend #2

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Hello } from './features/hello/hello';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Hello,
   ],
   imports: [
     BrowserModule,
