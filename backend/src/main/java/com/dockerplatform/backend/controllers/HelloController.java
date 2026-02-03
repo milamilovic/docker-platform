b00555ca56e34754726d3ef1aa31ca8006cce7b5
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2026-02-03 17:36:50 +0100

    add AuthGuard for route protection and update routing configuration #3

--- backend/src/main/java/com/dockerplatform/backend/controllers/HelloController.java
+++ backend/src/main/java/com/dockerplatform/backend/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.dockerplatform.backend.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public String hello() {
         return "Hello from backend!";
     }
