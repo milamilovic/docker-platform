88e455bc9b3d253f110c5f2b2de9b91b61b776ae
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2026-02-05 18:23:23 +0100

    add SystemSetupRunner to create super admin user on startup #23

---
+++ backend/src/main/java/com/dockerplatform/backend/configs/SystemSetupRunner.java
@@ -0,0 +1,36 @@
+package com.dockerplatform.backend.configs;
+
+import com.dockerplatform.backend.dto.UserDto;
+import com.dockerplatform.backend.models.enums.UserRole;
+import com.dockerplatform.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@Component
+public class SystemSetupRunner implements CommandLineRunner {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userService.findByUsername("superadmin") == null){
+            String rawPassword = UUID.randomUUID().toString();
+
+            UserDto dto = new UserDto();
+            dto.setUsername("superadmin");
+            dto.setPassword(rawPassword);
+            dto.setEmail("admin@admin.com");
+
+            userService.register(dto, UserRole.SUPER_ADMIN);
+            Files.writeString(Path.of("super_admin.txt"),rawPassword);
+            System.out.println("Super admin created!");
+        }
+    }
+}
